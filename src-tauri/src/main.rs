0f8e5f232efcbc47acc42e91b7a0d04b43541247
sinusphi <sinusphi.sq@gmail.com>
2026-02-16 23:25:32 +0100

    register tauri_plugin_dialog

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -10,6 +10,7 @@ fn get_uv_version() -> String {
 
 fn main() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_store::Builder::default().build())
         .invoke_handler(tauri::generate_handler![get_uv_version])
         .run(tauri::generate_context!())
